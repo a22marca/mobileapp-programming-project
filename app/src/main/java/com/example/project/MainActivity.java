6905bfa1e5460434d4b004299903afa16b842e1f
a22marca <128985931+a22marca@users.noreply.github.com>
2023-05-05 00:48:50 +0200

    Skapat och implementerat RecycleViewAdapter-klass

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,10 +2,19 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Island> islands;
+    private RecyclerViewAdapter recyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +23,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Testing RecyclerView
+        islands = new ArrayList<>(Arrays.asList(
+                new Island("Tristan da Cuhna", 123,"United kingdom", "Atlantic Ocean", "","Edinburgh of the Seven Seas",120,""),
+                new Island("Easter Island", 7750,"Chile", "Pacific Ocean", "https://en.wikipedia.org/wiki/Easter_Island","",164,""), //163.6 area
+                new Island("Pitcairn Islands", 47,"United kingdom", "Pacific Ocean", "","Adamstown",47,""),
+                new Island("Bouvet Island", 0,"Norway", "Atlantic Ocean", "","",49,""),
+                new Island("Kerguelen Islands", 45,"France", "Atlantic Ocean", "","Port-aux-Français",7125,"")
+        ));
+
+        recyclerViewAdapter = new RecyclerViewAdapter(this, islands, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(Island island) {
+                Log.d("Island:", island.getName());
+            }
+        });
+
+        RecyclerView view = findViewById(R.id.recyclerview_islands);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(recyclerViewAdapter);
     }
 }
---
+++ app/src/main/java/com/example/project/RecyclerViewAdapter.java
@@ -0,0 +1,67 @@
+package com.example.project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private List<Island> islands;
+    private LayoutInflater layoutInflater;
+    private OnClickListener onClickListener;
+
+    RecyclerViewAdapter(Context context, List<Island> islands, OnClickListener onClickListener) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.islands = islands;
+        this.onClickListener = onClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(layoutInflater.inflate(R.layout.recyclerview_island, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
+        holder.name.setText(islands.get(position).getName());
+        holder.government.setText(islands.get(position).getGovernment());
+        holder.ocean.setText(islands.get(position).getOcean());
+        holder.population.setText(String.valueOf(islands.get(position).getPopulation()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return islands.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView name;
+        TextView government;
+        TextView ocean;
+        TextView population;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            name = itemView.findViewById(R.id.island_name_textview);
+            government = itemView.findViewById(R.id.government_name_placeholder);
+            ocean = itemView.findViewById(R.id.ocean_location_textview);
+            population = itemView.findViewById(R.id.population_textview);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onClickListener.onClick(islands.get(getAdapterPosition()));
+        }
+    }
+
+    public interface OnClickListener {
+        void onClick(Island island);
+    }
+}
